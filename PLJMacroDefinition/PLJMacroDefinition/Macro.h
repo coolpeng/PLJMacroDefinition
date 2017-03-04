c7f57a875f916f4bee5306b3b18e3d3c37cba954
彭连进 <cool_plj@163.com>
2017-03-04 23:59:34 +0800

    init

---
+++ PLJMacroDefinition/PLJMacroDefinition/Macro.h
@@ -0,0 +1,20 @@
+//
+//  Macro.h
+//  PLJMacroDefinition
+//
+//  Created by Edward on 17/3/4.
+//  Copyright © 2017年 coolpeng. All rights reserved.
+//
+
+#ifndef Macro_h
+#define Macro_h
+
+//1. 尺寸相关
+// 屏幕
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
+#define kScreenSize [UIScreen mainScreen].bounds.size
+
+
+
+#endif /* Macro_h */
---
+++ PLJMacroDefinition/PLJMacroDefinition/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PLJMacroDefinition
+//
+//  Created by Edward on 17/3/4.
+//  Copyright © 2017年 coolpeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
